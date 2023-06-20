5dd42fc5ac26febaee5d00b67f041a22d22f3e9b
JohnKill87 <yankin.ilja2012@yandex.ru>
2023-06-20 18:25:53 +0300

    Домашнее задание 2.3(Введение в Maven и Spring Boot)

---
+++ src/main/java/pro/sky/lightcalculatordemo/LightCalculatorDemoApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.lightcalculatordemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightCalculatorDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightCalculatorDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/lightcalculatordemo/LightCalculatorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.lightcalculatordemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightCalculatorDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
